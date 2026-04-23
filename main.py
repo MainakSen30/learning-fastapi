15d3ea914ee653c6a0f57508867c5fb4597c8817
MainakSen30 <senmainak2001@gmail.com>
2026-04-23 23:06:58 +0530

    CRUD api practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from mockData import products
+from dtos import ProductDTO
 
 app = FastAPI()
 
@@ -33,3 +34,50 @@ def greet_user(request: Request):
         "greet": f"Hello {query_parameters.get("name")}, how are you",
         "age": f"You are {query_parameters.get("age")} years old"
     }
+
+##ways to send data to server
+## body data
+## headers (request headers)
+## query params
+
+##different types of http methods
+@app.post("/create_product")
+def create_product(body: ProductDTO):
+    product_data = body.model_dump()
+    products.append(product_data)
+    return {
+        "status": "product created successfully.",
+        "data": products
+    }
+
+## update
+@app.put("/update_product/{product_id}")
+def update_product(body: ProductDTO, product_id: int):
+    for index, p in enumerate(products):
+        if p.get("id") == product_id:
+            products[index] = body.model_dump()
+            return {
+                "status": "Product updated successfully...",
+                "data": body
+            }
+    return {
+        "status": "Product not found with the given id"
+    }
+
+
+@app.delete("/delete_product/{product_id}")
+def delete_product(product_id: int):
+    for index, p in enumerate(products):
+        if p.get("id") == product_id:
+            deleted_product = products.pop(index)
+            return {
+                "status": "product deleted successfully...",
+                "product": deleted_product
+            }
+    return {
+        "status": "Product not found with the given id"
+    }
+##pydantic
+##how to call different http methods - Any TOOL? Postman
+
+##how to validate data - DTOS
